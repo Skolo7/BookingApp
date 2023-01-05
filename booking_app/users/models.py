be69d46159734629a98c17804b9eba7de6754497
Skolo <philipskolo@gmail.com>
2023-01-05 20:02:37 +0100

    Added login & registration files

--- booking_app/users/models.py
+++ booking_app/users/models.py
@@ -1,12 +1,46 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class Person(models.Model):
-    name = models.CharField(max_length=15)
-    surname = models.CharField(max_length=30)
-    mail = models.EmailField(max_length=30)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30, unique=True)
     password = models.CharField(max_length=50)
-    login = models.CharField(max_length=25)
-    VIP = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     user_reservations = ArrayField
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'password', 'email']
+
+
+# class Person(models.Model):
+#     name = models.CharField(max_length=15)
+#     surname = models.CharField(max_length=30)
+#     mail = models.EmailField(max_length=30)
+#     password = models.CharField(max_length=50)
+#     login = models.CharField(max_length=25)
+#     VIP = models.BooleanField(default=False)
+#     user_reservations = ArrayField
 
--- booking_app/users/views.py
+++ booking_app/users/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserLoginForm, UserRegistrationForm
 
 
-def sign_in(request):
-    return render(request, 'sign-in.html')
+def login_view(request):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        user = authenticate(email=email, password=password)
+        login(request, user)
+        return redirect('home')
+    return render(request, 'users/login.html', {'form': form})
+
+
+
+

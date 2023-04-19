eb5b31141c49af3a7830f4aa2394da6220461a68
Skolo <philipskolo@gmail.com>
2023-04-19 14:47:55 +0200

    improved datepicker

--- booking_app/booking_app/views.py
+++ booking_app/booking_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
-from reservations.forms import ReservationForm
+from reservations.forms import ReservationForm, DateForm
+from reservations.models import Desk, Reservation
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 @login_required
@@ -14,10 +21,30 @@ def reserve(request):
         form = ReservationForm(request.POST)
         if form.is_valid():
             reservation = form.save()
-            return redirect('office')
+            start_date = form.cleaned_data['start_date']
+            end_date = form.cleaned_data['end_date']
+            available_desks = set(Desk.objects.all()) - {reserv.desk for reserv in
+                                                         Reservation.objects.filter(
+                                                             start_date__range=(start_date, end_date),
+                                                             end_date__range=(start_date, end_date)).select_related(
+                                                             'desk')}
+            context = {'all_desks': available_desks}
+            context['form'] = ReservationForm()
+            return render(request, 'reserve.html', context=context)
+            # return redirect('office')
     else:
         form = ReservationForm()
-    context = {}
+    print("Office Vie")
+
+    # start_date, end_date = timezone.now().date(), timezone.now().date()
+    #TODO maybe refactor in the future.
+    today = timezone.now().date()
+    available_desks = set(Desk.objects.all()) - {reserv.desk for reserv in
+                                                 Reservation.objects.filter(
+                                                     start_date__range=(today, today),
+                                                     end_date__range=(today, today)).select_related(
+                                                     'desk')}
+    context = {'all_desks': available_desks, 'today': today}
     context['form'] = ReservationForm()
     context['date_form'] = DateForm()
     return render(request, 'reserve.html', context=context)

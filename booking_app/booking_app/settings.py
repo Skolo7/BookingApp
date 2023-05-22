1385e4c6dbea58e45c235696c97c001b4cb1cf50
Skolo <philipskolo@gmail.com>
2023-05-22 16:38:47 +0200

    logout fix

--- booking_app/booking_app/settings.py
+++ booking_app/booking_app/settings.py
@@ -142,6 +142,7 @@ LOGIN_URL = 'login/'
 
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- booking_app/reservations/views.py
+++ booking_app/reservations/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
+# from reservations.forms import ReservationForm, DateForm, SingleReservationForm
+from models import Desk, Reservation
+from users/models import Account
+# from django.utils import timezone
+# from django.contrib.auth.decorators import login_required
+# import logging
 
-# Create your views here.
+def reservations(request):
+    reservations = Account.objects.all()
+    return render(request, '../reservations_template.html')

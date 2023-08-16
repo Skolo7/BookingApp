5bbf1eb84d400f3586188651ee380467344c829f
Skolo7 <filip.skolozynski@gmail.com>
2023-08-16 20:00:34 +0200

    repaired log out button

--- booking_app/manage.py
+++ booking_app/manage.py
@@ -1,4 +1,4 @@
-
+13
 
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
--- booking_app/users/views.py
+++ booking_app/users/views.py
@@ -40,6 +40,6 @@ def login_view(request):
 
 
 def logout_view(request):
-    if request.user.is_authenitcated:
+    if request.user.is_authenticated:
         logout(request)
         return redirect('login')

0399fb7e8e579d67a1d66408aa6e2aa82b55bf71
Skolo <philipskolo@gmail.com>
2024-05-04 12:41:59 +0200

    feat: GITHUB CI

--- booking_app/users/views/profile_view.py
+++ booking_app/users/views/profile_view.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, UpdateView
+
+from .forms import UserProfileForm
+from .models import UserProfile
 
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class UserProfileUpdateView(UpdateView):
+    model = UserProfile
+    form_class = UserProfileForm
     template_name = 'users/profile.html'
+
+    def get_object(self, queryset=None):
+        user, created = UserProfile.objects.get_or_create(user=self.request.user)
+        return user
+
+    def get_success_url(self):
+        return reverse_lazy('')
